f2be4ad7b723906bbd66a4f751e4de1e48d0c6b0
walzenbach <49525714+walzenbach@users.noreply.github.com>
2020-06-02 21:52:30 -0500

    Add files via upload

--- upcomingproducthunt_scrape.py
+++ upcomingproducthunt_scrape.py
@@ -1,10 +1,26 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
+
 url = 'https://www.producthunt.com/upcoming'
 response = requests.get(url)
 soup = BeautifulSoup(response.text, 'lxml')
+
+# create empty list
+product = []
+desc = []
+
 items = soup.find_all('div', class_='item_6a520')
 for i in items:
     itemName = i.find('div', class_='font_9d927 black_476ed large_db9e7 light_4a7e0 title_39c87 lineHeight_042f1 underline_57d3c').text
     itemDesc = i.find('div', class_='font_9d927 grey_bbe43 small_231df normal_d2e66 tagline_ce810 lineHeight_042f1 underline_57d3c').text
-    print('Product:' + itemName + ' desc:' + itemDesc)
+    product.append(itemName)
+    desc.append(itemDesc)
+
+# create dataframe from your lists
+df = pd.DataFrame(list(zip(product, desc)), 
+           columns =['product', 'desc'])
+
+df.to_csv('upcomingproducts.csv', index=False, header=True)
+
+#run terminal command cd ~/Desktop to execute file on desktop
